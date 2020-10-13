b2a041d8b3e345a8d9c81efa37e55caa4103fe4e
subdanial <subdanial@gmail.com>
2020-10-13 12:53:23 +0330

    image width config

--- config/filemanager.php
+++ config/filemanager.php
@@ -3,7 +3,7 @@ return [
     'base_route'      => 'admin/filemanager',
     'middleware'      => ['web', 'auth'],
     'allow_format'    => 'jpeg,jpg,png,gif,webp',
-    'max_size'        => 50000,
-    'max_image_width' => 10240,
+    'max_size'        => 5000,
+    'max_image_width' => 1024,
     'image_quality'   => 80,
 ];
