50bc3119993a334f920c816841b03a4a0748625c
subdanial <subdanial@gmail.com>
2020-10-27 14:03:22 +0330

    consulting_db

--- database/migrations/2020_10_27_061845_create_consultings_table.php
+++ database/migrations/2020_10_27_061845_create_consultings_table.php
@@ -15,6 +15,22 @@ class CreateConsultingsTable extends Migration
     {
         Schema::create('consultings', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('fullname')->nullable()->default('0');
+            $table->string('instagram')->nullable()->default('0');
+            $table->string('email')->nullable()->default('0');
+            $table->string('phone')->nullable()->default('0');
+            $table->string('site')->nullable()->default('0');
+            $table->text('activity')->nullable();
+            $table->text('services')->nullable();
+            $table->text('about_you')->nullable();
+            $table->text('about_work')->nullable();
+            $table->string('branded_before')->nullable()->default('0');
+            $table->string('branded_before_detail')->nullable()->default('0');
+            $table->string('fake_follower')->nullable()->default('0');
+            $table->string('count_follower')->nullable()->default('0');
+            $table->string('conscience')->nullable()->default('0');
+            $table->text('expectation')->nullable();
+
             $table->timestamps();
         });
     }
